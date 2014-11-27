c4cd1546c32c7b59fa82a67a9c00b1f69c70c37f
startcaft <51418996@qq.com>
2014-11-27 17:30:22 +0800

    WCF服务实例激活模式

---
+++ Wcf.ServiceContext.Mode.Client/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wcf.ServiceContext.Mode.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------单调服务--------------");
+            //1,单调服务代理实例化，每次调用操作，都会创建不同的服务实例
+            WCFServicePerCall.WCFServiceClient perCallProxy = new WCFServicePerCall.WCFServiceClient();
+            //循环调用服务
+            for (int i = 0; i < 2; i++)
+            {
+                perCallProxy.SayHello();
+            }
+            perCallProxy.Close();
+
+
+            Console.WriteLine("--------------服务会话--------------");
+            //2，服务会话代理实例化，一个客户端代理对应一个服务实例
+            WCFServicePerSession.WCFServiceClient perSessionProxy = new WCFServicePerSession.WCFServiceClient();
+            for (int i = 0; i < 2; i++)
+            {
+                perSessionProxy.SayHello();
+            }
+            perSessionProxy.Close();//关闭
+
+            WCFServicePerSession.WCFServiceClient perSessionProxy2 = new WCFServicePerSession.WCFServiceClient();
+            for (int i = 0; i < 2; i++)
+            {
+                perSessionProxy2.SayHello();
+            }
+            perSessionProxy2.Close();
+
+
+            Console.WriteLine("--------------单例服务模式--------------");
+            //3，单例服务代理 实例化，也叫单件模式。所有的服务只有一个服务实例
+            WCFServiceSingleTon.WCFServiceClient singletonProxy = new WCFServiceSingleTon.WCFServiceClient();
+            for (int i = 0; i < 2; i++)
+            {
+                singletonProxy.SayHello();
+            }
+            singletonProxy.Close();
+
+            WCFServiceSingleTon.WCFServiceClient singletonProxy2 = new WCFServiceSingleTon.WCFServiceClient();
+            for (int i = 0; i < 2; i++)
+            {
+                singletonProxy2.SayHello();
+            }
+            singletonProxy2.Close();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Wcf.ServiceContext.Mode.Host/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Wcf.ServiceContext.Mode.Service;
+
+namespace Wcf.ServiceContext.Mode.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1，单调服务WCFServicePerCall
+            ServiceHost perCall = new ServiceHost(typeof(WCFServicePerCall));
+            if (perCall.State != CommunicationState.Opening)
+            {
+                perCall.Open();
+            }
+            Console.WriteLine("WCFServicePerCall主机已经开启，服务状态为:{0}", perCall.State);
+
+            //2，会话服务WCFServicePerSession
+            ServiceHost perSession = new ServiceHost(typeof(WCFServicePerSession));
+            if (perSession.State != CommunicationState.Opening)
+            {
+                perSession.Open();
+            }
+            Console.WriteLine("WCFServicePerSession主机已经开启，服务状态为:{0}", perSession.State);
+
+            //3，单例服务WCFServiceSingleTon
+            ServiceHost singleTon = new ServiceHost(typeof(WCFServiceSingleTon));
+            if (singleTon.State != CommunicationState.Opening)
+            {
+                singleTon.Open();
+            }
+            Console.WriteLine("WCFServiceSingleTon主机已经开启，服务状态为:{0}", singleTon.State);
+
+            Console.Read();
+
+
+            //关闭
+            perCall.Close();
+            perSession.Close();
+            singleTon.Close();
+        }
+    }
+}
