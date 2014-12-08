9b8727f0f010aeddd5e0af15c2a2ba2e24b57e40
startcaft <51418996@qq.com>
2014-12-08 16:26:25 +0800

    UriTemplate

---
+++ Wcf_Web_Http.BasicWebProgramming/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wcf_Web_Http.BasicWebProgramming
+{
+    //服务契约
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebGet]
+        string EchoWithGet(string s);
+
+        [OperationContract]
+        [WebInvoke]//WebInvoke默认是POST调用,可通过其Method属性来修改成PUT,DELETE调用
+        string EchoWithPost(string s);
+    }
+
+    //服务契约的实现
+    public class Service : IService
+    {
+        public string EchoWithGet(string s)
+        {
+            return "You said " + s;
+        }
+
+        public string EchoWithPost(string s)
+        {
+            return "You said " + s;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //根据服务类，以及基地址来初始化一个服务宿主
+            WebServiceHost host = new WebServiceHost(typeof(Service), new Uri("Http://localhost:8000/"));
+            try
+            {
+                //初始化服务终结点,切记绑定的协议为WebHttpBinding
+                ServiceEndpoint ep = host.AddServiceEndpoint(typeof(IService), new WebHttpBinding(), "");
+                host.Open();
+                using (ChannelFactory<IService> cf = new ChannelFactory<IService>(new WebHttpBinding(), "http://localhost:8000"))
+                {
+                    //添加一个服务的终结点行为---WebHttpBehavior实例
+                    cf.Endpoint.EndpointBehaviors.Add(new WebHttpBehavior());
+                    IService channel = cf.CreateChannel();
+
+                    string s;
+
+                    Console.WriteLine("Calling EchoWithGet via HTTP GET:");
+                    s = channel.EchoWithGet("Hello,world");
+                    Console.WriteLine(" OutPut:{0}", s);
+
+                    Console.WriteLine("----------------------------------");
+                    Console.WriteLine("This can also be accomplished by navigating to");
+                    Console.WriteLine("http://localhost:8000/EchoWithGet?s=Hello, world!");
+                    Console.WriteLine("in a web browser while this sample is running.");
+
+                    Console.WriteLine("----------------------------------");
+                    Console.WriteLine("Calling EchoWithPost via HTTP POST: ");
+                    s = channel.EchoWithPost("Hello, world");
+                    Console.WriteLine("   Output: {0}", s);
+                    Console.WriteLine("");
+
+                    Console.WriteLine("Press <ENTER> to terminate");
+                    Console.ReadLine();
+                }
+            }
+            catch (CommunicationException cex)
+            {
+                Console.WriteLine("An exception occurred: {0}", cex.Message);
+                host.Abort();
+            }
+            finally
+            {
+                host.Close();
+            }
+        }
+    }
+}
---
+++ Wcf_Web_Http_UriTemplate/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wcf_Web_Http_UriTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * UriTeplate标识统一资源标识符(URI)模版的类,它继承自System.Object
+             * 其位于System.ServiceModel程序集中(System.ServiceModel.dll)
+             * 
+             * URI模版，可以定义一组结构相似的URI。模版由两部分组成，即路径和查询。路径由一些列反斜杠(/)分隔的段组成。
+             * 每个段可都可以具有文本值，变量值(书写在大括号中({})且被选址为仅与一个段的内容匹配)或必须出现在路径末端的通配符(书写为型号*，与"路径的其余部分"匹配)。
+             * 查询表达式可以完全忽略，如果出现表达式，则它指定一组无序的名称/值对，查询表达式的元素可以是文本对(?x=2)
+             * 也可以是变量对(?x={val})。不允许使用不成对的值
+             */
+
+            UriTemplate template = new UriTemplate("weather/{state}/{city}?forecast={day}");
+            Uri prefix = new Uri("http://localhost");
+
+            Console.WriteLine("路径片段所有的变量名称:");
+            foreach (string  name in template.PathSegmentVariableNames)
+            {
+                Console.WriteLine("     {0}", name);
+            }
+
+            Console.WriteLine("查询表达式中的变量名称:");
+            foreach (string name in template.QueryValueVariableNames)
+            {
+                Console.WriteLine("     {0}", name);
+            }
+
+            Uri positionaUri = template.BindByPosition(prefix, "Washington", "Redmond", "Today");
+            NameValueCollection parameters = new NameValueCollection();
+            parameters.Add("state", "Washington");
+            parameters.Add("city", "Redmond");
+            parameters.Add("day", "Today");
+            Uri nameUri = template.BindByName(prefix, parameters);
+
+            Uri fullUri = new Uri("http://localhost/weather/Washington/Redmond?forecast=today");
+            UriTemplateMatch result = template.Match(prefix, fullUri);
+
+            Console.WriteLine("Matching {0} to {1}", template.ToString(), fullUri.ToString());
+
+            if (result != null)
+            {
+                foreach (string variableName in result.BoundVariables.Keys)
+                {
+                    Console.WriteLine("     {0}:{1}", variableName, result.BoundVariables[variableName]);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
