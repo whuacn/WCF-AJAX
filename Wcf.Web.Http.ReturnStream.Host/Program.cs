cddbbc0ffef5332714a13cc21799539d2c6f9a17
startcaft <51418996@qq.com>
2014-12-08 17:06:53 +0800

    Wcf_Web_Http的Stream操作

---
+++ Wcf.Web.Http.ReturnStream.Host/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using Wcf.Web.Http.ReturnStream.Service;
+
+namespace Wcf.Web.Http.ReturnStream.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //服务基地址
+            string baseAddress = "http://" + Environment.MachineName + ":8000/Service";
+            using (ServiceHost host = new ServiceHost(typeof(MyService),new Uri(baseAddress)))
+            {
+                host.AddServiceEndpoint(typeof(IImageServer), new WebHttpBinding(), "").EndpointBehaviors.Add(new WebHttpBehavior());
+                host.Open();
+
+                Console.WriteLine("Service is running");
+                Console.Write("Press ENTER to close the host");
+                Console.ReadLine();
+            }
+        }
+    }
+}
